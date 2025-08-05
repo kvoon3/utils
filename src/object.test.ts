c151b7569c7fd0f3e4050136381c65c3872e94a4
kvoon <kvoon@duck.com>
2025-08-05 21:22:14 +0800

    feat(object): getDeepValue

--- src/object.test.ts
+++ src/object.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { clearNull } from './object.ts'
+import { clearNull, getDeepValue } from './object.ts'
 
 describe('object', () => {
   it('clearNull', () => {
@@ -10,5 +10,31 @@ describe('object', () => {
 
     expect(obj).toEqual({ bar: 'baz' })
     expect(obj.foo).toBe(undefined)
+  })
+
+  it('get deep value', () => {
+    expect(getDeepValue({
+      foo: {
+        bar: {
+          baz: {
+            key: [42],
+          },
+        },
+      },
+    })).toEqual([42])
+
+    expect(getDeepValue({
+      foo: {
+        bar: {
+          baz: {
+            foo: ['foo'],
+            bar: 'bar',
+          },
+        },
+      },
+    })).toEqual({
+      foo: ['foo'],
+      bar: 'bar',
+    })
   })
 })
--- src/object.ts
+++ src/object.ts
@@ -1,3 +1,5 @@
+import { isObject, objectKeys } from '@antfu/utils'
+
 export function clearNull<T extends object>(obj: T): T {
   for (const key in obj) {
     if (obj[key] === null)
@@ -5,4 +7,22 @@ export function clearNull<T extends object>(obj: T): T {
   }
 
   return obj as any
+}
+
+export function getDeepValue(obj: object): any {
+  const get = (obj: object): any => {
+    const keys = objectKeys(obj)
+    if (keys.length === 1) {
+      const value = obj[keys[0]]
+      if (isObject(value) && !Array.isArray(value))
+        return get(value)
+      else
+        return value
+    }
+    else {
+      return obj
+    }
+  }
+
+  return get(obj)
 }
